18d79858be129fd59ac09cd9ee7d362b8a369d04
Lintangms14 <merayusukmalintang@gmail.com>
2023-05-25 12:30:23 +0700

    int main

--- duoble_linked_list/duoble_linked_list.cpp
+++ duoble_linked_list/duoble_linked_list.cpp
@@ -162,3 +162,45 @@ void DoubleLinkedList::searchdata() {
 	}
 }
 
+int main() {
+	DoubleLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all records in the ascending order of the roll numbers" << endl;
+			cout << "4. View all records in the descending order of roll numbers" << endl;
+			cout << "5. Search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			char ch;
+			cin >> ch;
+
+			switch (ch) {
+			case '1':
+				obj.addnode();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.ascending();
+				break;
+			case '4':
+				obj.descending();
+				break;
+			case '5':
+				obj.searchdata();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+			}
+		}
+		catch (exception& e) {
+			cout << "check for the values enterned." << endl;
+		}
+	}
+}
