e99b1491c7730bf6dd792139d3b3918cce61701d
Lintangms14 <merayusukmalintang@gmail.com>
2023-05-25 10:27:25 +0700

    class duoblelinkedlist private dan public

--- duoble_linked_list/duoble_linked_list.cpp
+++ duoble_linked_list/duoble_linked_list.cpp
@@ -9,3 +9,19 @@ public:
 	node* next;
 	node* prev;
 };
+
+class DuobleLinkedList {
+private:
+	node* START;
+public:
+
+	DuobleLinkedList();
+	void addnode();
+	bool search(int rollNo, node** previous, node** current);
+	bool deletenode(int rollNo);
+	bool listempty();
+	void ascending();
+	void descending();
+	void hapus();
+	void searchdata();
+};
