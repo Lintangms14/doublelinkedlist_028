2ee15ee1aad6025e78ebe5f1369749a264f96618
Lintangms14 <merayusukmalintang@gmail.com>
2023-05-25 12:30:06 +0700

    fix error

--- duoble_linked_list/duoble_linked_list.cpp
+++ duoble_linked_list/duoble_linked_list.cpp
@@ -162,14 +162,3 @@ void DoubleLinkedList::searchdata() {
 	}
 }
 
-int main() {
-	DoubleLinkedList obj;
-	while (true) {
-		try {
-			cout << "\nMenu" << endl;
-			cout << "1. Add a record to the list" << endl;
-			cout << "2. Delete a record from the list" << endl;
-			cout << "3. View all records in the ascending order of the roll numbers" << endl;
-		}
-	}
-}
